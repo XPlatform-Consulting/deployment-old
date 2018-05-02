b5d4a576c7091f31003bfd2513409cd80888abf2
Jacob Sarver <jacob.sarver96@gmail.com>
2018-05-02 11:59:31 -0600

    Removed defaults on vars and added AWS region var.

--- modules/aws-portal-services/examples/complete/variables.tf
+++ modules/aws-portal-services/examples/complete/variables.tf
@@ -1,33 +1,36 @@
+variable "aws_region" {
+
+}
 variable "vpc_id" {
-  default = "vpc-de3d97a5"
+
 }
 variable "private_subnet_1" {
-  default = "subnet-c522d78f"
+
 }
 variable "private_subnet_2" {
-  default = "subnet-bfba9be2"
+
 }
 variable "master_password" {
-  default = "portal123"
+
 }
 variable "master_username" {
-  default = "portal"
+
 }
 variable "vidi_master_password" {
-  default = "vidispine123"
+
 }
 variable "vidi_master_username" {
-  default = "vidispine"
+
 }
 variable "vidi_broker_password" {
-  default = "vidispine123"
+
 }
 variable "vidi_broker_username" {
-  default = "vidispine"
+
 }
 variable "activiti_master_username" {
-  default = "activiti"
+
 }
 variable "activiti_master_password" {
-  default = "activiti123"
+
 }
