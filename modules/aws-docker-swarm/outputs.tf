868427d0e791e675c31773f8c5c0f4bf984a824f
Jacob Sarver <jacob.sarver96@gmail.com>
2018-05-15 15:34:15 -0600

    Added additional outputs to aws-docker-swarm module

--- modules/aws-docker-swarm/outputs.tf
+++ modules/aws-docker-swarm/outputs.tf
@@ -1,6 +1,18 @@
-output "MASTERS" {
+output "MASTERS_PRIVATE_IP" {
   value = "${module.aws-ec2-master.private_ip}"
 }
-output "WORKERS" {
+output "WORKERS_PRIVATE_IP" {
   value = "${module.aws-ec2-worker.private_ip}"
+}
+output "MASTERS_PUBLIC_IP" {
+  value = "${module.aws-ec2-master.public_ip}"
+}
+output "WORKERS_PUBLIC_IP" {
+  value = "${module.aws-ec2-worker.public_ip}"
+}
+output "masters_id" {
+  value = "${module.aws-ec2-master.id}"
+}
+output "workers_id" {
+  value = "${module.aws-ec2-worker.id}"
 }
